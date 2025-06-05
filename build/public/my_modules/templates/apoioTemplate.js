c447fae9fa089e4e43b45635b4fcff68f25dc6c6
botechia-erika <24.01905-4@maua.br>
2025-06-05 08:15:32 -0300

    pagina colaboradores e pagina esqueleto do blog adicionadas

--- build/public/my_modules/templates/apoioTemplate.js
+++ build/public/my_modules/templates/apoioTemplate.js
@@ -1,8 +1,67 @@
+const dataSupporters = [
+    {
+        id: 1,
+        name: 'CADES Ipiranga',
+        imgUrl: '/../assets/brand/apoiadores/apoio001-cades-ipiranga.png',
+        link: 'https://capital.sp.gov.br/web/ipiranga/w/cades_ip/32342'
+    },
+    {
+        id: 2,
+        name: 'Instituto CHÃO',
+        imgUrl: '/../assets/brand/apoiadores/apoio002-instituto-chao.png',
+        link: 'https://www.institutochao.org/'
+    },
+    {
+        id: 3,
+        name: 'UMAPAZ',
+        imgUrl: '/../assets/brand/apoiadores/apoio003-umapaz.png',
+        link: 'https://capital.sp.gov.br/web/meio_ambiente/w/umapaz/sobre_a_umapaz/243'
+    },
+    {
+        id: 4,
+        name: 'CADES VILA MARIANA',
+        imgUrl: '/../assets/brand/apoiadores/apoio004-cades-vila-mariana.png',
+        link: 'https://capital.sp.gov.br/web/vila_mariana/w/cadesvm/31201'
+    },
+    {
+        id: 5,
+        name: 'GEPEM',
+        imgUrl: '/../assets/brand/apoiadores/apoio005-GEPEM.png',
+        link: 'https://www.gepem.org/grupo/sobre/'
+    }
+];
+
+const supportersList = (dataList) => {
+    return dataList.map((s) => (
+        `
+        <div class="col-12 col-sm-6 col-lg-4 mb-4 d-flex align-items-stretch">
+            <div class="card shadow-sm w-100 h-100 border-0">
+                <a href="${s.link}" target="_blank" rel="noopener noreferrer" class="text-decoration-none">
+                    <img class="card-img-top p-4" src="${s.imgUrl}" alt="${s.name}" style="max-height: 180px; object-fit: contain;">
+                    <div class="card-body text-center">
+                        <h5 class="card-title mb-2">${s.name}</h5>
+                    </div>
+                </a>
+            </div>
+        </div>
+        `
+    )).join('');
+};
+
+const lista = supportersList(dataSupporters);
+
 export const apoioTemplate = `
-<div class="container">
-    <h2> COLABORAÇÕES AO PROJETO</h2>
+<div class="container bannerHome my-5">
+    <h2 class="fw-bold text-center mb-2">APOIOS E COLABORADORES</h2>
+    <p class="lead text-center mb-4">Conheça a lista de nossos parceiros</p>
 </div>
-<div class="container">
-    <h3>CONHEÇA A LISTA DE COLABORADORES</h3>
-</div>
+<section class="container mb-5">
+    <div class=" rounded-4 shadow-sm p-4">
+        <h3 class="fw-bold text-center mb-3">Apoios e Colaboradores</h3>
+        <p class="text-center mb-4">Conheça quem participa construindo a usina junto ao coletivo. Clique no logo para visitar os links dos nossos parceiros!</p>
+        <div class="row justify-content-center">
+            ${lista}
+        </div>
+    </div>
+</section>
 `
--- build/public/my_modules/templates/blogTemplate.js
+++ build/public/my_modules/templates/blogTemplate.js
@@ -1,8 +1,74 @@
 export const blogTemplate = `
-<div class="container">
-    <h2> BLOG </h2>
+<div class="container bannerHome">
+<h2>Blog e Notícias da Usina Eco-Cultural</h2>
+<p>Conheça nossa lista de Posts de Interesse e Notícias sobre a Usina!</p>
 </div>
-<div class="container">
-    <h3>POSTS LISTA</h3>
-</div>
+<section class="container">
+ <div class="row bg-light p-3 mb-3 rounded">
+            <div class="col-md-8">
+                <h3 class="pb-4 mb-4 font-italic border-bottom">
+                    Nossos Posts
+                </h3>
+                <div class="blog-post">
+                    <h2 class="blog-post-title">New feature</h2>
+                    <p class="blog-post-meta">December 14, 2013 by <a href="#">Chris</a></p>
+
+                    <p>Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aenean
+                        lacinia bibendum nulla sed consectetur. Etiam porta sem malesuada magna mollis euismod. Fusce
+                        dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit
+                        amet risus.</p>
+                    <ul>
+                        <li>Praesent commodo cursus magna, vel scelerisque nisl consectetur et.</li>
+                        <li>Donec id elit non mi porta gravida at eget metus.</li>
+                        <li>Nulla vitae elit libero, a pharetra augue.</li>
+                    </ul>
+                    <p>Etiam porta <em>sem malesuada magna</em> mollis euismod. Cras mattis consectetur purus sit amet
+                        fermentum. Aenean lacinia bibendum nulla sed consectetur.</p>
+                    <p>Donec ullamcorper nulla non metus auctor fringilla. Nulla vitae elit libero, a pharetra augue.
+                    </p>
+                </div><!-- /.blog-post -->
+
+                <nav class="blog-pagination">
+                    <a class="btn btn-outline-primary" href="#">Older</a>
+                    <a class="btn btn-outline-secondary disabled" href="#" tabindex="-1" aria-disabled="true">Newer</a>
+                </nav>
+
+            </div>
+
+            <aside class="col-md-4">
+                <div class="p-4 mb-3 bg-light rounded">
+                    <h4 class="font-italic">Sobre o blog...</h4>
+                    <p class="mb-0">Escrito e mantido por <em>voluntários</em> que separam os melhores conteúdos do momento para você se interar de nosso trabalho, aproveite para o dar <strong>LIKES</strong> e <strong>COMENTAR</strong> o <em>feedback</em> do post.</p>
+                </div>
+
+                <div class="p-4">
+                    <h4 class="font-italic">Archives</h4>
+                    <ol class="list-unstyled mb-0">
+                        <li><a href="#">March 2014</a></li>
+                        <li><a href="#">February 2014</a></li>
+                        <li><a href="#">January 2014</a></li>
+                        <li><a href="#">December 2013</a></li>
+                        <li><a href="#">November 2013</a></li>
+                        <li><a href="#">October 2013</a></li>
+                        <li><a href="#">September 2013</a></li>
+                        <li><a href="#">August 2013</a></li>
+                        <li><a href="#">July 2013</a></li>
+                        <li><a href="#">June 2013</a></li>
+                        <li><a href="#">May 2013</a></li>
+                        <li><a href="#">April 2013</a></li>
+                    </ol>
+                </div>
+
+                <div class="p-4">
+                    <h4 class="font-italic">Elsewhere</h4>
+                    <ol class="list-unstyled">
+                        <li><a href="#">GitHub</a></li>
+                        <li><a href="#">Twitter</a></li>
+                        <li><a href="#">Facebook</a></li>
+                    </ol>
+                </div>
+            </aside>
+
+        </div><!-- /.row -->
+</section>
 `
